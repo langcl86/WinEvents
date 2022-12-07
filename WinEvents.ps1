51cfb5198e9c001171bbcb1058d8f723ce320d5c
langcl86 <96951049+langcl86@users.noreply.github.com>
2022-12-06 22:42:10 -0500

    Update WinEvents.ps1

--- WinEvents.ps1
+++ WinEvents.ps1
@@ -10,9 +10,11 @@ function main {
     $lvlTypes.Add("Information", 4);
 
     $mainForm = New-Object System.Windows.Forms.Form;
+    $mainForm.FormBorderStyle = [System.Windows.Forms.FormBorderStyle]::FixedSingle;
+    $mainForm.StartPosition = [System.Windows.Forms.FormStartPosition]::CenterScreen;
     $mainForm.Width = 500;
     $mainForm.Height = 550;
-    $mainForm.FormBorderStyle = [System.Windows.Forms.FormBorderStyle]::FixedSingle;
+    $mainForm.Text = "WinEvents";
 
     $logLabel = New-Object System.Windows.Forms.Label;
     $logLabel.Text = "Log Name: ";
@@ -59,7 +61,6 @@ function main {
     addCtrl($idLabel);
 
     $idField = New-Object System.Windows.Forms.TextBox;
-    $idField.Width = 200;
     $idField.Location = "110, 125";
     addCtrl($idField);
 
@@ -72,6 +73,8 @@ function main {
     $datePicker1 = New-Object System.Windows.Forms.DateTimePicker;
     $datePicker1.Location = "110, 165";
     $datePicker1.Value = (Get-Date).AddDays("-1");
+    $datePicker1.MaxDate = (Get-Date);
+    $datePicker1.Add_ValueChanged({$datePicker2.MinDate = $datePicker1.Value;});
     addCtrl($datePicker1);
 
     $dateLabel2 = New-Object System.Windows.Forms.Label;
@@ -81,9 +84,12 @@ function main {
     addCtrl($dateLabel2);
 
     $datePicker2 = New-Object System.Windows.Forms.DateTimePicker;
+    $datePicker2.Width = 200;
     $datePicker2.Location = "110, 205";
     $datePicker2.Value = (Get-Date);
-    $mainForm.Controls.Add($datePicker2);
+    $datePicker2.MinDate = $datePicker1.Value;
+    $datePicker2.MaxDate = (Get-Date);
+    addCtrl($datePicker2);
 
     $maxLabel = New-Object System.Windows.Forms.Label;
     $maxLabel.Text = "Max Events: ";
@@ -100,13 +106,13 @@ function main {
     $btnSubmit = New-Object System.Windows.Forms.Button;
     $btnSubmit.Location = "10, 280";
     $btnSubmit.Text = "Submit";
-    $btnSubmit.Add_MouseDown({  $rtbResult.Text = buildCmd; });
+    $btnSubmit.Add_Click({  $rtbResult.Text = buildCmd; });
     addCtrl($btnSubmit);
 
     $btnCancel = New-Object System.Windows.Forms.Button;
     $btnCancel.Location = "110, 280";
     $btnCancel.Text = "Cancel";
-    $btnCancel.Add_MouseDown({ $mainForm.Close(); });
+    $btnCancel.Add_Click({ $mainForm.Close(); });
     addCtrl($btnCancel);
 
     $rtbResult = New-Object System.Windows.Forms.RichTextBox;
@@ -118,19 +124,26 @@ function main {
     $btnRun = New-Object System.Windows.Forms.Button;
     $btnRun.Location = "10, 480";
     $btnRun.Text = "Run";
-    $btnRun.Add_MouseDown({ runCMD; });
+    $btnRun.Add_Click({ runCMD; });
     addCtrl($btnRun);
 
     $btnCopy = New-Object System.Windows.Forms.Button;
     $btnCopy.Location = "110, 480";
     $btnCopy.Text = "Copy";
-    $btnCopy.Add_MouseDown({ Set-Clipboard $rtbResult.Text; });
+    $btnCopy.Add_Click({ Set-Clipboard $rtbResult.Text; });
     addCtrl($btnCopy);
 
     $mainForm.ShowDialog() | Out-Null;
 }
 
-function addCtrl ($type) { $mainForm.Controls.Add($type); }
+function addCtrl ($type) { 
+    try {
+        $mainForm.Controls.Add($type);
+    }
+    catch {
+        newError "Failed to add control $($type.GetType().Name)";
+    }
+}
 
 function updateProviders {
     try {
@@ -138,8 +151,12 @@ function updateProviders {
         $providerList | Where-Object {$_.LogLinks.LogName -EQ $logField.Text} | ForEach-Object {$providerField.Items.Add($_.Name);} | Out-Null;
     }
     catch {
-        [System.Windows.Forms.MessageBox]::Show("Failed to update provider list`r`n$($_.Exception.Message)","Error", [System.Windows.Forms.MessageBoxButtons]::OK, [System.Windows.Forms.MessageBoxIcon]::Error);
+        newError "Failed to update provider";
     }
+}
+
+function newError ($msg) {
+        [System.Windows.Forms.MessageBox]::Show("$msg`r`n$($_.Exception.Message)","Error", [System.Windows.Forms.MessageBoxButtons]::OK, [System.Windows.Forms.MessageBoxIcon]::Error);
 }
 
 function notNull ($str) {
@@ -174,16 +191,22 @@ function buildCmd {
 }
 
 function runCMD {
+    $cmd = $rtbResult.Text;
+
+    if($cmd.Length -lt 1) { 
+        newError "Command text is empty.";
+        return;
+     }
+        
     [System.IO.FileInfo]$ps =  Join-Path ([System.Environment]::SystemDirectory) "WindowsPowerShell\v1.0\powershell.exe";
     $outfile = Join-Path ([System.IO.Path]::GetTempPath()) "WinEvents_output.ps1";
-    $cmd = $rtbResult.Text;
     $cmd | Out-File $outfile
 
     try {
         Start-Process $ps -ArgumentList "-NoLogo -NoExit -File $outfile" | Out-Null;
     }
     catch {
-        [System.Windows.Forms.MessageBox]::Show("Powershell command failed`r`n$($_.Exception.Message)","Error", [System.Windows.Forms.MessageBoxButtons]::OK, [System.Windows.Forms.MessageBoxIcon]::Error);
+        newError "Powershell failed to start.";
     }
 }
 
